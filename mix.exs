9dbfeb8eb268b83a48ac1d992e1639b6c291f54c
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-12-15 16:39:28 -0700

    release 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PoeApi.Mixfile do
   def project do
     [app: :poe_api,
      description: "high-productivity collection of tools and practices for rapidly writing production-ready applications",
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
