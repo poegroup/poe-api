d056412c336e30747f811fb5124e14ecb1b43b98
bbassett <bassett.brandon@gmail.com>
2018-01-19 14:49:57 -0700

    use hex for plug_wait1

--- mix.exs
+++ mix.exs
@@ -29,8 +29,7 @@ defmodule PoeApi.Mixfile do
      {:mazurka_plug, "~> 0.1.0"},
      {:plug, "~> 1.3.0"},
      {:plug_x_forwarded_proto, "~> 0.1.0"},
-     # {:plug_wait1, "~> 0.2.3"},
-     {:plug_wait1, github: "wait1/plug_wait1"},
+     {:plug_wait1, "~> 0.2.3"},
      {:poison, "3.1.0"},
      {:simple_secrets, "~> 1.0.0"},]
   end
